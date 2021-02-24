41e05003f394a241465991c17f0cae2cda9d4834
Rapira159-afk <Rapira.2013@mail.ru>
2021-02-24 15:48:17 +0300

    Shop7фото

--- src/STR_1/Osnova/1Rezume/Rezumeimg/IMG.jsx
+++ src/STR_1/Osnova/1Rezume/Rezumeimg/IMG.jsx
@@ -5,7 +5,7 @@ import w from './w.jpg';
 function Imgr1 () {
     return (
         <div>
-            <div><div><img src={w} width= '900' height= '500'/></div> </div>
+            <div><div><img src={w} width= '500' height= '500' /></div> </div>
         </div>
     )
 };
