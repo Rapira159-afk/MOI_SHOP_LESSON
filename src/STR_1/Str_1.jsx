f129c3cd819e239b16fa5417154a43d5438352aa
Rapira159-afk <Rapira.2013@mail.ru>
2021-03-26 15:09:14 +0300

    04Нерабоч

--- src/STR_1/Str_1.jsx
+++ src/STR_1/Str_1.jsx
@@ -4,13 +4,14 @@ import './Str_1.css'
 import { Shapka } from './Shapka/Shapka';
 
 import {BrowserRouter} from "react-router-dom";
+import { Osnova } from './Osnova/Osnova';
 
 
 function Str_1(props) {
   return (
     <div className='STR_1'>
       <div className='Shapka'><Shapka /></div>
- 
+      <div className='Osnova'><Osnova /></div>
 
     </div>
   );
