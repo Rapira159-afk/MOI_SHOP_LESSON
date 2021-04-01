f2a76e82893eba68c92cda1e3ceb6feed8b8820b
Rapira159-afk <Rapira.2013@mail.ru>
2021-04-01 10:55:39 +0300

    6

--- src/STR_1/Osnova/Category/Osnov1/1/1.jsx
+++ src/STR_1/Osnova/Category/Osnov1/1/1.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-
-
+import q1 from './q1.jpg';
+import q2 from './q2.jpg';
+import q3 from './q3.jpg';
 function Categ1(props) {
     return (
         <div>
-            <div>ФОТО Женской одежды</div>
+            <div>ФОТО Женской одежды
+            <div>Блузка</div>
+            <div> <img src={q1} width= '200' height= '250' /></div>
+            <div>1000р</div>  
+            <div>Лифон</div> 
+            <div><img src={q2} width= '200' height= '250' /></div>
+            <div>1500р</div>
+            <div>Маечка</div> 
+            <div><img src={q3} width= '200' height= '250' /></div>
+            <div>1300р</div>
+            </div>
         </div>
     )
 };
 
 export {Categ1};
+
+
